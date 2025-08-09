96a7e3c595e220e2cdcbe218ae062b4d0b5a4c5a
MegaTKC <59368582+MegaTKC@users.noreply.github.com>
2025-08-09 11:04:16 -0700

    Delete expansion/compatibility/Towny directory

--- expansion/compatibility/Towny/build.gradle.kts
+++
@@ -1,8 +0,0 @@
-repositories {
-    maven("https://nexus.sirblobman.xyz/proxy-jitpack/")
-}
-
-dependencies {
-    compileOnly("com.github.TownyAdvanced:Towny:0.100.3.2")
-    compileOnly("com.github.TownyAdvanced:FlagWar:0.6.2")
-}
