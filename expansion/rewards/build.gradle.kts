be8bbd7113f1fab56694c79a491229af79b218e7
MegaTKC <59368582+MegaTKC@users.noreply.github.com>
2025-08-09 10:37:59 -0700

    Update build.gradle.kts

--- expansion/rewards/build.gradle.kts
+++ expansion/rewards/build.gradle.kts
@@ -1,7 +1,7 @@
 java {
-    sourceCompatibility = JavaVersion.VERSION_1_8
-    targetCompatibility = JavaVersion.VERSION_1_8
-    toolchain.languageVersion.set(JavaLanguageVersion.of(8))
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+    toolchain.languageVersion.set(JavaLanguageVersion.of(17))
 }
 
 repositories {
